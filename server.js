f6fffb9f733f67420ae33527a03444a1b4974f44
Sam Cackett <samcackett@uk.ibm.com>
2016-03-25 21:42:35 +0000

    Add basic routing to different apps

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var proxy = httpProxy.createProxyServer();
 httpProxy.createProxyServer({target:'http://localhost:9005'}).listen(8000);
 
 app.use('/', express.static(__dirname + '/views'));
+app.use('/angular2', express.static(__dirname + '/views/angular2'));
 app.listen(9005, function() {
     console.log('app listening on port 9005');
 });
