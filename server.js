919488a48b437939e83af559c79dfc8908282067
Sam Cackett <samcackett@uk.ibm.com>
2016-03-25 22:41:11 +0000

    Add bower_components to be served by node Test Angular

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ httpProxy.createProxyServer({target:'http://localhost:9005'}).listen(8000);
 
 app.use('/', express.static(__dirname + '/views'));
 app.use('/angular', express.static(__dirname + '/dist'));
+// TODO: Dev mode only. Or compile to dist
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.listen(9005, function() {
     console.log('app listening on port 9005');
 });
