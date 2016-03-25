8b71dfe5a0979d82618fb6e44bac4bcc8a6e6529
Sam Cackett <samcackett@uk.ibm.com>
2016-03-25 21:29:26 +0000

    Add basic proxy server

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 /**
- * Created by sam on 25/03/16.
+ * Create a proxy server for returning multiple applications
+ * @type {*|exports|module.exports}
  */
+var httpProxy = require('http-proxy'),
+    http = require('http');
+
+var proxy = httpProxy.createProxyServer();
+
+// Set up proxy server
+httpProxy.createProxyServer({target:'http://localhost:9005/angular2'}).listen(8000);
+
+// Create server
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+    res.end();
+}).listen(9005);
